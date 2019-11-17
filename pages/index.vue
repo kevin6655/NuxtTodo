7430650f1a9b936bb1a100abd0842de7592b6c7b
kevin6655 <daitanj65@gmail.com>
2019-11-18 00:34:11 +0900

    [Add]insert,find,delete処理追加

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,22 @@
       <h1 class="title">TodoApp</h1>
       <h2 class="subtitle">Nuxt TodoApp</h2>
       <div>
-        <input type="text" id="contents" name="content" />
-        <button>Save</button>
-        <button>Find</button>
+        <input
+          type="text"
+          id="contents"
+          name="content"
+          v-model="content"
+          @focus="set_flg"
+        />
+        <button @click="insert">Save</button>
+        <button @click="find">Find</button>
       </div>
       <div>
         <ul>
-          <li v-for="(todo, index) in todos" :key="index">
+          <li v-for="(todo, index) in findTodo" :key="index">
             <span>{{ todo.content }}</span>
             <span>{{ todo.createtime }}</span>
+            <span><button @click="remove(todo)">Remove</button></span>
           </li>
         </ul>
       </div>
@@ -26,16 +33,56 @@ import { mapState } from 'vuex'
 import Logo from '~/components/Logo.vue'
 
 export default {
-  date: () => {
+  data() {
     return {
-      content: ''
+      content: '',
+      find_flg: false
     }
   },
   components: {
     Logo
   },
   computed: {
-    ...mapState(['todos'])
+    ...mapState(['todos']),
+    findTodo() {
+      // find_flgがTrueの時に走る
+      if (this.find_flg) {
+        // 配列定義
+        const arr = []
+        const data = this.todos
+        // todosの中身をforeachで回して検索を行う。
+        data.forEach((element) => {
+          // contentの内容を大文字小文字を統一し、比較を行う。
+          if (element.content.toLowerCase() === this.content.toLowerCase()) {
+            arr.push(element)
+          }
+        })
+        // 配列を返す
+        return arr
+      } else {
+        return this.todos
+      }
+    }
+  },
+  methods: {
+    insert() {
+      // commit()で引数に実行したいメソッド名とマッピングする内容を入れる
+      this.$store.commit('insert', { content: this.content })
+      this.content = ''
+    },
+    find() {
+      this.find_flg = true
+    },
+    set_flg() {
+      // trueだったflgをfalseにしてcontentを空白にする。
+      if (this.find_flg) {
+        this.find_flg = false
+        this.content = ''
+      }
+    },
+    remove(todo) {
+      this.$store.commit('remove', todo)
+    }
   }
 }
 </script>
