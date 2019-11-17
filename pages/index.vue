1e85fb6e635ca6108225b7c12c7f6040862657c9
kevin6655 <daitanj65@gmail.com>
2019-11-17 16:28:12 +0900

    [Add]2019/11/17 Input And Button Add

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,12 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        TodoApp
-      </h1>
-      <h2 class="subtitle">
-        Nuxt TodoApp
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <h1 class="title">TodoApp</h1>
+      <h2 class="subtitle">Nuxt TodoApp</h2>
+      <div>
+        <input type="text" id="contents" name="content" />
+        <button>Save</button>
+        <button>Find</button>
       </div>
     </div>
   </div>
